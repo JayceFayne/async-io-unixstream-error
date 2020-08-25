0ed7fde9f455797f1c84c0d100f2551fcf8dec93
Jayce Fayne <jayce.fayne@mailbox.org>
2020-08-25 16:00:35 +0200

    1.5 swayipc

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
-use async_io::Async;
-use futures_lite::future;
-use std::io::Write;
-use std::os::unix::net::UnixStream;
-use std::thread;
+use swayipc::Connection;
 
-fn main() {
-    let mut handles = Vec::new();
-    for _ in 1..100 {
-        let handle = thread::spawn(|| {
-            future::block_on(async {
-                Async::<UnixStream>::connect("/run/user/1000/sway-ipc.1000.871.sock")
-                    .await?
-                    .write_with_mut(|io| io.write(&[]))
-                    .await
-            })
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        if let Err(error) = handle.join().unwrap() {
-            println!("{:?}", error)
-        }
+#[async_std::main]
+async fn main() {
+    for _ in 1..100_000 {
+        Connection::new()
+            .await
+            .unwrap()
+            .get_bar_ids()
+            .await
+            .unwrap();
     }
 }
