3296f9a7b9c7512d9882495aebdcf6892b471930
Jayce Fayne <jayce.fayne@mailbox.org>
2020-08-27 23:53:54 +0200

    fix async-io connect

--- src/test/async_io.rs
+++ src/test/async_io.rs
@@ -5,11 +5,22 @@ use std::os::unix::net::UnixStream;
 
 pub fn run() {
     future::block_on(async {
-        Async::<UnixStream>::connect(SOCKET_PATH)
-            .await
-            .unwrap()
-            .write(MSG)
-            .await
-            .unwrap()
+        connection().await.unwrap().write(MSG).await.unwrap();
     });
+}
+
+async fn connection() -> std::io::Result<Async<UnixStream>> {
+    use async_io::Timer;
+    use std::io::ErrorKind;
+    use std::time::Duration;
+
+    loop {
+        match Async::<UnixStream>::connect(SOCKET_PATH).await {
+            Err(err) if err.kind() == ErrorKind::NotConnected => {
+                Timer::after(Duration::from_millis(100)).await;
+            }
+            Err(err) => return Err(err),
+            Ok(conn) => return Ok(conn),
+        }
+    }
 }
